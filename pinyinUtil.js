adf4bf5d6de08db7b6aaf79fc3e4b345c9dbf67b
liuxianan <937925941@qq.com>
2016-10-19 15:31:19 +0800

    增加多音字的识别，修改文件结构

--- pinyinUtil.js
+++ pinyinUtil.js
@@ -36,7 +36,15 @@
 			// 如果导入了 pinyin_dict_withtone.js
 			if(window.pinyin_dict_withtone)
 			{
-				dict.withtone = pinyin_dict_withtone.split(',');
+				dict.withtone = {};
+				var temp = pinyin_dict_withtone.split(',');
+				for(var i=0, len = temp.length; i<len; i++)
+				{
+					// 这段代码耗时28毫秒左右，对性能影响不大，所以一次性处理完毕
+					dict.withtone[String.fromCharCode(i + 19968)] = temp[i]; // 这里先不进行split(' ')，因为一次性循环2万次split比较消耗性能
+				}
+
+				// 拼音 -> 汉字
 				if(window.pinyin_dict_notone)
 				{
 					// 对于拼音转汉字，我们优先使用pinyin_dict_notone字典文件
@@ -52,6 +60,7 @@
 					for(var i=0, len = notone.length; i<len; i++)
 					{
 						hz = String.fromCharCode(i + 19968); // 汉字
+						// = aaa[i];
 						py = notone[i].split(' '); // 去掉了声调的拼音数组
 						for(var j=0; j<py.length; j++)
 						{
@@ -64,39 +73,38 @@
 		},
 		/**
 		 * 根据汉字获取拼音，如果不是汉字直接返回原字符
-		 * @param str 要转换的汉字
+		 * @param chinese 要转换的汉字
 		 * @param splitter 分隔字符，默认用空格分隔
 		 * @param withtone 返回结果是否包含声调，默认是
 		 * @param polyphone 是否支持多音字，默认否
 		 */
-		getPinyin: function(str, splitter, withtone, polyphone)
+		getPinyin: function(chinese, splitter, withtone, polyphone)
 		{
-			if(!str || /^ +$/g.test(str)) return '';
+			if(!chinese || /^ +$/g.test(chinese)) return '';
 			splitter = splitter == undefined ? ' ' : splitter;
 			withtone = withtone == undefined ? true : withtone;
 			polyphone = polyphone == undefined ? false : polyphone;
 			var result = [];
 			if(dict.withtone) // 优先使用带声调的字典文件
 			{
-				for (var i=0, len = str.length; i < len; i++)
+				for (var i=0, len = chinese.length; i < len; i++)
 				{
-					var unicode = str.charCodeAt(i);
-					var py = dict.withtone[unicode-19968];
-					if(py)
+					var pinyin = dict.withtone[chinese[i]];
+					if(pinyin)
 					{
-						if(!polyphone) py = py.replace(/ .*$/g, ''); // 如果不需要多音字
-						if(!withtone) py = this.removeTone(py); // 如果不需要声调
+						if(!polyphone) pinyin = pinyin.replace(/ .*$/g, ''); // 如果不需要多音字
+						if(!withtone) pinyin = this.removeTone(pinyin); // 如果不需要声调
 					}
-					result.push(py || str.charAt(i)); 
+					result.push(pinyin || chinese[i]); 
 				}
 			}
 			else if(dict.notone) // 使用没有声调的字典文件
 			{
 				if(withtone) console.warn('pinyin_dict_notone 字典文件不支持声调！');
 				if(polyphone) console.warn('pinyin_dict_notone 字典文件不支持多音字！');
-				for (var i=0, len = str.length; i < len; i++)
+				for (var i=0, len = chinese.length; i < len; i++)
 				{
-					var temp = str.charAt(i);
+					var temp = chinese.charAt(i);
 					result.push(dict.notone[temp] || temp); 
 				}
 			}
@@ -105,7 +113,11 @@
 				throw '抱歉，未找到合适的拼音字典文件！';
 			}
 			if(!polyphone) return result.join(splitter);
-			else return handlePolyphone(result, ' ', splitter);
+			else
+			{
+				if(window.pinyin_dict_polyphone) return parsePolyphone(chinese, result, splitter, withtone);
+				else return handlePolyphone(result, ' ', splitter);
+			}
 		},
 		/**
 		 * 获取汉字的拼音首字母
@@ -220,6 +232,56 @@
 			result = temp;
 		}
 		return simpleUnique(result);
+	}
+
+	/**
+	 * 根据词库找出多音字正确的读音
+	 * 这里只是非常简单的实现，效率和效果都有一些问题
+	 * 推荐使用第三方分词工具先对句子进行分词，然后再匹配多音字
+	 * @param chinese 需要转换的汉字
+	 * @param result 初步匹配出来的包含多个发音的拼音结果
+	 * @param splitter 返回结果拼接字符
+	 */
+	function parsePolyphone(chinese, result, splitter, withtone)
+	{
+		var poly = window.pinyin_dict_polyphone;
+		var max = 7; // 最多只考虑7个汉字的多音字词，虽然词库里面有10个字的，但是数量非常少，为了整体效率暂时忽略之
+		var temp = poly[chinese];
+		if(temp) // 如果直接找到了结果
+		{
+			temp = temp.split(' ');
+			for(var i=0; i<temp.length; i++)
+			{
+				result[i] = temp[i] || result[i];
+				if(!withtone) result[i] = pinyinUtil.removeTone(result[i]);
+			}
+			return result.join(splitter);
+		}
+		for(var i=0; i<chinese.length; i++)
+		{
+			temp = '';
+			for(var j=0; j<max && (i+j)<chinese.length; j++)
+			{
+				if(!/^[\u2E80-\u9FFF]+$/.test(chinese[i+j])) break; // 如果碰到非汉字直接停止本次查找
+				temp += chinese[i+j];
+				var res = poly[temp];
+				if(res) // 如果找到了多音字词语
+				{
+					res = res.split(' ');
+					for(var k=0; k<=j; k++)
+					{
+						if(res[k]) result[i+k] = withtone ? res[k] : pinyinUtil.removeTone(res[k]);
+					}
+					break;
+				}
+			}
+		}
+		// 最后这一步是为了防止出现词库里面也没有包含的多音字词语
+		for(var i=0; i<result.length; i++)
+		{
+			result[i] = result[i].replace(/ .*$/g, '');
+		}
+		return result.join(splitter);
 	}
 
 	// 简单数组去重
